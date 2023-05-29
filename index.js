8782ca72e4e9947b4e45f8251d634b102553f8f0
ragnarrok <magna.ragnarrok@gmail.com>
2023-05-29 18:53:00 +0200

    added extra dependencies

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
+import config from 'config'
+import nodemailer from 'nodemailer'
 import * as mqtt from "mqtt";
 
 console.log(chalk.red("Hello ") + chalk.green("World") + chalk.red("!"));
+const mqttserverconfig = config.get("MQTTServer.host");
+console.log(chalk.blue(mqttserverconfig));
